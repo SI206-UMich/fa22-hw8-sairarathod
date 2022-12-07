aa326455ee776261df03b20419471ba00fd42ded
Saira Rathod <sairarathod@Sairas-MacBook-Pro.local>
2022-12-07 14:38:18 -0500

    2

--- HW8.py
+++ HW8.py
@@ -12,28 +12,18 @@ def get_restaurant_data(db_filename):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_filename)
     cur = conn.cursor()
-
-    cur.execute("SELECT * FROM restaurants JOIN categories ON restaurants.catgory_id = categories.id")
+    rest_list=[]
+    cur.execute("SELECT name, building, category, rating FROM restaurants JOIN buildings ON restaurants.building_id = buildings.id JOIN categories ON restaurants.category_id = categories.id")
     this = cur.fetchall()
-    print(this)
-    # cur.execute("SELECT name, rating FROM restaurants")
-    # this = cur.fetchall()
-    # rest = []
-    # for i in this:
-    #     rest.append(i[0])
-    # print(rest)
-
-    # cur.execute("SELECT category FROM categories")
-    # this = cur.fetchall()
-    # type = []
-    # for i in this:
-    #     type.append(i[0])
-
-    # cur.execute("SELECT building FROM buildings")
-    # this = cur.fetchall()
-    # where = []
-    # for i in this:
-    #     where.append(i[0])
+    
+    for i in this:
+        dict ={}
+        dict['name'] = i[0]
+        dict['category'] = i[2]
+        dict['building'] = i[1]
+        dict['rating'] = i[3]
+        rest_list.append(dict)
+    return rest_list
     pass
 
 def barchart_restaurant_categories(db_filename):
@@ -56,7 +46,7 @@ def highest_rated_category(db_filename):#Do this through DB as well
 
 #Try calling your functions here
 def main():
-    get_restaurant_data("South_U_Restaurants.db")
+    # get_restaurant_data("South_U_Restaurants.db")
 
     pass
 
